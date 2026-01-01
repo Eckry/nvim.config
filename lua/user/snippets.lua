03ed8908d4e228e347c10a7080db85169d16ce70
Eckry <112786854+Eckry@users.noreply.github.com>
2025-12-31 19:20:48 -0600

    Add ordered set snippet

--- lua/user/snippets.lua
+++ lua/user/snippets.lua
@@ -26,3 +26,11 @@ ls.add_snippets('all', {
     "}"
   })
 })
+
+ls.add_snippets('all', {
+  s('init_ordered_set', t {
+    "#include <ext/pb_ds/assoc_container.hpp>",
+    "#include <ext/pb_ds/tree_policy.hpp>",
+    "typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;"
+  })
+})
